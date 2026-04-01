6ec7782b0b0fbbcfadfb9ee564f0de53b61a3ee5
kwdie <dieg.torresc@duocuc.cl>
2026-04-01 12:02:39 -0300

    primer commit

---
+++ src/main/java/com/ejemplo/anashei/AnasheiApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.anashei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnasheiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnasheiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/anashei/AnasheiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.anashei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnasheiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
